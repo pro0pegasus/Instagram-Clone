41828e1033dd297b19812071f1712ee18cf86bd2
Yassine Borak <yassineborak@gmail.com>
2025-12-24 17:19:08 +0100

    building fix

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const posts = [
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "public/images/avatar-vangogh.jpg",
-        post: "public/images/post-vangogh.jpg",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -27,8 +27,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "public/images/avatar-courbet.jpg",
-        post: "public/images/post-courbet.jpg",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -36,8 +36,8 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "public/images/avatar-ducreux.jpg",
-        post: "public/images/post-ducreux.jpg",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
-	plugins: [],
+
   	base: '/', // ensure assets load from the root
 })
